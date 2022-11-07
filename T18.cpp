729371c4ffe33c967ea44c6a0650c18779b662bd
yangjinghua <1692047690@qq.com>
2022-11-07 22:10:12 +0800

    多态(动态多态) （程序运行期间才能决定到底调用那个类的函数 == 动态多态） 什么是多态       1 父类的引用指向子类对象       2 同一个方法有不同的实现       3 重载（静态多态）和重写(动态多态)       virtual string getLayout();//虚函数（java版的抽象类）       virtual string getLayout() = 0;//纯虚函数（java版的抽象类）       全纯虚函数（java的接口）       模版函数类似java的范型template<typename TT>       vector  向量 容器       栈 先进后出 后进先出       queue 队列       优先级队列       // 优先级队列 默认排序 默认做了从大到小的封装       // 隐士代码  priority_queue<int,vector<int>,less<int>>       set 集合使用排序和重写对象       map集合 谓词入门       太累了卷不动了。提代码22年11。07

--- T18.cpp
+++ T18.cpp
@@ -1,8 +1,29 @@
 //
 // Created by 杨敬华 on 2022/11/6.
-//
+//谓词
 #include "iostream"
+#include "set"
+#include "algorithm"
 using namespace std;
+class showAction{
+public:
+    void operator()(int content){
+        cout<<"自定义反函数"<<endl;
+    }
+
+};
+void showAction(int content){
+    cout<<"一元谓词"<<endl;
+}
 int main18(){
+    set<int> setVar;
+    setVar.insert(10);
+    setVar.insert(20);
+    setVar.insert(30);
+    setVar.insert(40);
+    setVar.insert(50);
+    setVar.insert(60);
+    setVar.insert(70);
+    for_each(setVar.begin(), setVar.end(),showAction);
     return 0;
 }
--- T19.cpp
+++ T19.cpp
@@ -1,8 +1,22 @@
 //
 // Created by 杨敬华 on 2022/11/6.
-//
+//函数适配器
 #include "iostream"
+#include "set"
 using namespace std;
-int main19(){
+int main(){
+    set<string,less<string>>setvar;
+    setvar.insert("AAAA");
+    setvar.insert("BBBB");
+    setvar.insert("CCCC");
+    for(auto it = setvar.begin();it!=setvar.end();it++){
+        cout<<*it<<endl;
+    }
+    set<string,less<string>>::iterator itetrtorRes = find_if(setvar.begin(),setvar.end(), bind2nd(equal_to<string>(),"CCCC"));
+    if (itetrtorRes!=setvar.end()){
+        cout<<"ok"<<endl;
+    } else{
+        cout<<"no"<<endl;
+    }
     return 0;
 }
