036ca63aee5ffdfa873af59ddcaa2ac50f01ecb9
yangjinghua <1692047690@qq.com>
2022-11-09 23:10:59 +0800

    多态(动态多态) （程序运行期间才能决定到底调用那个类的函数 == 动态多态） 什么是多态       1 父类的引用指向子类对象       2 同一个方法有不同的实现       3 重载（静态多态）和重写(动态多态)       virtual string getLayout();//虚函数（java版的抽象类）       virtual string getLayout() = 0;//纯虚函数（java版的抽象类）       全纯虚函数（java的接口）       模版函数类似java的范型template<typename TT>       vector  向量 容器       栈 先进后出 后进先出       queue 队列       优先级队列       // 优先级队列 默认排序 默认做了从大到小的封装       // 隐士代码  priority_queue<int,vector<int>,less<int>>       set 集合使用排序和重写对象       map集合 谓词入门       太累了卷不动了。提代码22年11。07       C++多线程

--- T23.cpp
+++ T23.cpp
@@ -2,7 +2,21 @@
 // Created by 杨敬华 on 2022/11/6.
 //
 #include "iostream"
+#include "pthread.h"
+#include "unistd.h"
 using namespace std;
-int main23(){
+void * tunTask(void * pvoid){
+    int number = *static_cast<int *>(pvoid);
+    cout<<"一部线程"<<endl;
+    for (int i = 0; i < 10; ++i) {
+        cout<<"run:"+i<<endl;
+        sleep(1);
+    }
+}
+int main(){
+    int number = 999;
+    pthread_t pthreadID;
+    pthread_create(&pthreadID,0,tunTask,&number);
+    sleep(10);
     return 0;
 }
